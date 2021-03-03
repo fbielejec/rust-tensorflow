cf4fc7167d234d1577fc893e76312bb07e4fc80d
filip <fbielejec@gmail.com>
2021-03-03 15:27:50 +0100

    compute scores

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 mod config;
+mod utils;
 
-#[macro_use]
-extern crate quick_error;
-extern crate polars;
+// #[macro_use]
+// extern crate quick_error;
+// extern crate polars;
 
 use config::{Config, Load};
 use log::{debug, info, error};
-use polars::chunked_array::ChunkedArray;
-use polars::datatypes::{Utf8Type, ArrowDataType};
+// use polars::chunked_array::ChunkedArray;
+// use polars::datatypes::{Utf8Type, ArrowDataType};
 use polars::prelude::*;
-use rayon::prelude::*;
 use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::path::Path;
 use std::result::Result;
-use std::sync::Arc;
 use std::time::{Instant, Duration as StdDuration};
-use tensorflow::{Graph, Session, SessionOptions, SessionRunArgs, Tensor, SavedModelBundle };
-use tokio::{time::delay_for, fs::File as TokioFile, io};
+use utils::print_type_of;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+// #[tokio::main]
+fn main() -> Result<(), Box<dyn Error>> {
 
     let config = Config::load();
 
@@ -36,13 +34,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Running with config {:?}", config);
 
-
     let data = read_csv (&config.data_path)?;
+    let scores = calculate_scores (&data);
 
+    debug!("Head of the data:");
+    info!("@@@ {:#?}", &scores);
 
-    info!("@@@ {:?}", data);
-
-
+    // data.add_column ();
 
 
     info!("Done");
@@ -51,26 +49,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 
 
-
-
-
-
+/// calculates the scores as a linear function of predictors
+fn calculate_scores (data : &DataFrame) -> Result<DataFrame, PolarsError> {
+    let mut scores_column = &(&(&data ["video_slash_normalized_progress"] + &data ["video_slash_liked_qmark_"]) + &data ["video_slash_commented_qmark_"]) / 3f32;
+    scores_column.rename("scores");
+    DataFrame::new(vec![data ["video_slash_id"].clone (),
+                        data ["user_slash_id"].clone (),
+                        scores_column])
+}
 
 fn read_csv (path : &String) -> Result<DataFrame, PolarsError> {
-
     let file = File::open(&path).unwrap_or_else(|_| panic!("Failed to open file: {}", path));
 
-    // enforce schema
-    let video_id = Field::new("video_slash_id", ArrowDataType::Utf8, false);
-    let user_id = Field::new("user_slash_id", ArrowDataType::Utf8, false);
-    let video_progress = Field::new("video_slash_normalized_progress", ArrowDataType::Float32, false);
-    let video_is_liked = Field::new("video_slash_liked_qmark_", ArrowDataType::Int16, false);
-    let video_is_commented = Field::new("video_slash_commented_qmark_", ArrowDataType::Int16, false);
+    let video_id = Field::new("video_slash_id", DataType::Utf8);
+    let user_id = Field::new("user_slash_id", DataType::Utf8);
+    let video_progress = Field::new("video_slash_normalized_progress", DataType::Float32);
+    let video_is_liked = Field::new("video_slash_liked_qmark_", DataType::Float32);
+    let video_is_commented = Field::new("video_slash_commented_qmark_", DataType::Float32);
+
     let schema : Arc<Schema> = Arc::new (Schema::new(vec![video_id,
                                                           user_id,
                                                           video_progress,
                                                           video_is_liked,
                                                           video_is_commented]));
+
     CsvReader::new(file)
         .with_schema(schema)
         .has_header(true)
